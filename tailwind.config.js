bd4936b33dd32f400854679d9e6078114386635d
mercklyzer <mercklyzer@gmail.com>
2026-05-27 20:28:17 +0800

    fix: fix Tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			colors: {
-				green: { 800: "#1B6747" },
-			},
-		},
-	},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        green: { 800: "#1B6747" },
+      },
+    },
+  },
 };
